fee98c5c5ca9661b8575bbca6f421b30b89ebfcc
Sanghee Kim <sangheestyle@gmail.com>
2014-12-08 00:24:19 -0700

    RFC: use brain data class

--- calc_cov_network.py
+++ calc_cov_network.py
@@ -1,18 +1,13 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import covariance
-import os
+from brain_data import BrainData
 
 
 raw_data = json.load(open('data/data_v2.json'))
-data = raw_data['subj1_16regions_1845time']
-
-# model = covariance.GraphLasso(alpha=0.001)
-model = covariance.GraphLassoCV()
-model.fit(data)
-os.exit()
-cov_ = model.covariance_
+b1 = BrainData(raw_data)
+b1.calc_covariance(method="graphlassocv", values="cov")
+cov_ = b1.model.covariance_
 
 plt.figure(figsize=(8, 8))
 vmax = cov_.max()
@@ -23,8 +18,8 @@ plt.title('covariance by GraphLassoCV()')
 
 plt.figure(figsize=(4, 3))
 plt.axes([.2, .15, .75, .7])
-plt.plot(model.cv_alphas_, np.mean(model.grid_scores, axis=1), 'o-')
-plt.axvline(model.alpha_, color='.5')
+plt.plot(b1.model.cv_alphas_, np.mean(b1.model.grid_scores, axis=1), 'o-')
+plt.axvline(b1.model.alpha_, color='.5')
 plt.title('Model selection')
 plt.ylabel('Cross-validation score')
 plt.xlabel('alpha')
